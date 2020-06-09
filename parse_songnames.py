27623552f52c0dff6b0d863d101468a79f3144e9
NikoKalbitzer <niko.kalbitzer@st.oth-regensburg.de>
2020-06-09 18:47:06 +0200

    parsing names

--- parse_songnames.py
+++ parse_songnames.py
@@ -3,10 +3,16 @@ import re
 
 songlist = open("songList.txt", "r")
 for line in songlist:
-    #print(line)
-    file = os.path.basename(line)
-    if file[0] != ".":  # filter out hidden names
+    # print(line)
+    path, file = os.path.split(line)
+    if file[0] != ".":  # filter out hidden files
         file = os.path.splitext(file)[0]  # remove file extension
-        file = re.sub("^(\d+\s-\s)", "", file) # remove song number
+        file = re.sub("^(\d+\s-\s)", "", file)  # remove song number
+        interpreter = re.split("/", path)[2]  # pathname like: Music/DSD Multichannel/Interpreter/Albumname/CD/Songname
+        if re.sub(" ","", interpreter).lower() != "variousartists": #only remove artist from name if not various artists
+            file = re.split("(\s-\s)", file)[-1] # remove the interpreter from name if present
+        else:
+            print("not stripped")
+        print(interpreter)
         print(file)
 songlist.close()
